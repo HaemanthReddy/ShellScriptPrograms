66a5fd67a19d9f1670710e52284a768914b6756c
HaemanthReddy <77066601+HaemanthReddy@users.noreply.github.com>
2021-01-21 20:33:39 +0530

    Add files via upload

---
+++ day5sol2_3.sh
@@ -0,0 +1,14 @@
+echo -n "Enter number : "
+read n
+
+case "$echo" in
+        "1") echo  "one "
+	;;
+        "10") echo "Ten "
+	;;
+        "100") echo "Hundred "
+	;;
+        "1000") echo "Thousand "
+	;;
+    esac
+
