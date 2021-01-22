2c9b06108e45317ebbfccd9c41d3d8ac30e298b8
HaemanthReddy <77066601+HaemanthReddy@users.noreply.github.com>
2021-01-22 12:43:35 +0530

    Add files via upload

---
+++ array/repeatedtwice.sh
@@ -0,0 +1,13 @@
+for(( index=1; index<100; index++ ))
+do
+number=$index
+
+remainder=`expr $index % 10 `
+number=`expr $index / 10 `
+
+if [ $remainder -eq $number ]
+then
+    echo "To print the repeated twice:" $index
+    array=($index)
+fi
+done
